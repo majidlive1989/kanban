73279815b19f4e778a0e458ea3506fc7eef3104f
m.ali <majid.live@gmail.com>
2025-10-16 20:41:52 +0330

    v1.4

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
 import "./style.css";
 const addEl = document.getElementById("newtask") as HTMLButtonElement;
 const modalEl = document.getElementById("modal") as HTMLElement;
+const blurEl = document.getElementById("blurmj") as HTMLElement;
 
+addEl.addEventListener("click", () => {
+  modalEl.classList.remove("hidden");
+  blurEl.classList.remove("hidden");
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    modalEl.classList.add("hidden");
+    blurEl.classList.add("hidden");
+  }
+});
+blurEl.addEventListener("click", () => {
+  blurEl.classList.add("hidden");
+  modalEl.classList.add("hidden");
+});
